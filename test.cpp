8e559b19facdfbb694c3b6b577f7dceae08a9801
palakbhawsar5 <94602436+palakbhawsar5@users.noreply.github.com>
2021-11-18 14:21:24 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    public:
+    char name[];
+    string enrollmentNo;
+    int contactNo;
+};
+void student{
+    cout<<"enter name";
+    cin>>name;
+    cout<<"enter enrollmentNo";
+    cin>>enrollmentNo;
+    cout<<"enter contact number";
+    cin>>contactNo;
+    return 0;
+}
